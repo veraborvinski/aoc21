510ed61fcaf330b78698c27dcdf06e9082abbd8c
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2021-12-07 08:25:46 +0100

    Create 2.c

---
+++ day7/2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int getNum();
+
+int main(){
+
+	printf("%d\n", getNum());
+	return 0;
+}
+
+int getNum(){
+    int num;
+    int lowestsofar;
+    int prevsum;
+    for (int i = 1; i < 1500; ++i)
+    {
+        int sum = 0;
+        FILE *fp = fopen( "input.txt", "r" );
+        if (fp!=NULL) 
+        {
+            while (fscanf(fp, "%d, ", &num)==1)
+            {
+                for (int j = 0; j <= abs(num-i); ++j)
+                {
+                    sum += j;
+                }   
+            }
+            fclose (fp);
+        }
+        if(sum < prevsum){
+            lowestsofar = sum;
+        }
+        prevsum = sum;
+    }
+    return lowestsofar;
+}
